5a260d5ee08f45e7d89c10b54199256a359a3f8a
Azer Panganiban <zerpanganiban@gmail.com>
2020-12-03 14:18:12 +0800

    Initial project add

---
+++ src/main/java/com/springframework/azerpetclinic/AzerPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.azerpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzerPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzerPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/azerpetclinic/AzerPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.azerpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzerPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
